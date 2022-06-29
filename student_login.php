c304aa54c8261c2f5f9473f09e27b616164731e9
Rahul Reghu <87755997+rrahul32@users.noreply.github.com>
2022-06-30 00:49:28 +0530

    Update student_login.php
    Signup prblm

--- student_login.php
+++ student_login.php
@@ -50,7 +50,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
         </div>
         <div class="row text-center">
             <p>
-                New user? <a href="company_login.php">Sign Up</a>
+                New user? <a href="student_signup.php">Sign Up</a>
             </p>
         </div>
         <div class="row">
