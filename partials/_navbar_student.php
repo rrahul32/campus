e54f05dd3159ec75c506dffb3f762bad9a426413
rrahul32 <rahulreghu32@gmail.com>
2022-07-07 00:44:33 +0530

    job search

--- partials/_navbar_student.php
+++ partials/_navbar_student.php
@@ -1,25 +1,51 @@
 <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-  <div class="container-fluid">
-  <a class="navbar-brand p-0" href="/campus">
-        <div class="row">
-    <img class="col" src="/campus/images/index.png" alt="CRP" width="50" height="50" class="d-inline-block align-text-top">
-      <div class="col">Campus Recruitment Portal<br><center><em><small>Student</small></em></center></div>
-        </div></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="/campus/student">Search Jobs</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/campus/logout.php">Logout</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/campus/aboutus.php">About Us</a>
-        </li>
-      </ul>
+    <div class="container-fluid">
+        <a class="navbar-brand p-0" href="/campus/student">
+            <div class="row">
+                <img class="col" src="/campus/images/index.png" alt="CRP" width="50" height="50" class="d-inline-block align-text-top">
+                <div class="col">Campus Recruitment Portal<br>
+                    <center><em><small>Student</small></em></center>
+                </div>
+            </div>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/campus/student" >Search Jobs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/campus/student">Companies</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/campus/aboutus.php">About Us</a>
+                </li>
+
+            </ul>
+        </div>
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+                <a class="nav-link pe-5" href="#" id="notifications" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="/campus/images/notifications.svg" alt="Account" height="25" width="25">
+                </a>
+                <div class="dropdown-menu dropdown-menu-dark dropdown-menu-end" aria-labelledby="notifications">
+                    <div>
+                        <div class="text-center">No Notifications</div>
+                    </div>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link pe-5" href="#" id="account" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="/campus/images/account.svg" alt="Account" height="25" width="25">
+                </a>
+                <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end" aria-labelledby="account">
+                    <li><a class="dropdown-item " href="/campus/student/profile.php">Profile</a></li>
+                    <li><a class="dropdown-item " href="/campus/student/settings.php">Settings</a></li>
+                    <li><a class="dropdown-item " href="/campus/logout.php">Logout</a></li>
+                </ul>
+            </li>
+        </ul>
     </div>
-  </div>
 </nav>
--- student/index.php
+++ student/index.php
@@ -1,16 +1,52 @@
 <?php
 session_start();
 if (isset($_SESSION['loggedin'])) {
-     if ($_SESSION['type'] == "company")
+    if ($_SESSION['type'] == "company")
         header("Location: /campus/company");
     else if ($_SESSION['type'] == "admin")
         header("Location: /campus/admin");
-}
-$pageTitle="Dashboard";
-include_once $_SERVER['DOCUMENT_ROOT'].'/campus/partials/_template.php';
+    }
+    // echo var_dump($_GET);
+    if(isset($_GET['search']))
+    {
+        include_once $_SERVER['DOCUMENT_ROOT'].'/campus/partials/config.php';
+        $search=$_GET['search'];
+        $sql="SELECT * FROM `job` WHERE `jname`='$search';";
+        $result=mysqli_query($conn,$sql);
+        $rows=mysqli_fetch_row($result);
+        echo var_dump($rows);
+    }
+$pageTitle = "Search for Jobs: Campus Recruitment Management System";
+include_once $_SERVER['DOCUMENT_ROOT'] . '/campus/partials/_template.php';
 ?>
+<div class="container-flex border">
 
+    <form class=" col-6 mx-auto py-5" action="" method="GET">
+        <div class="row">
+            <div class="form-floating form-control-sm col-9 mx-auto">
+                <input type="text" class="form-control" id="search" placeholder="Company" name="search" required>
+                <label for="search" style="left:auto;">Enter job title</label>
+            </div>
+            <!-- <div class="form-floating col">
+<input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email" required>
+      <label for="floatingInput">Email address</label>
+</div> -->
+            <div class="d-flex col-3 py-1">
+                <input class="btn btn-primary" type="submit" name="submit" value="Find Jobs">
+            </div>
+        </div>
+    </form>
+</div>
+<div class="container px-auto text-center p-2">
+    <div class="row">
+        <h3>Recent Search</h3>
+    </div>
+    <div class="row" id="recentSearch">
+
+    </div>
+
+</div>
 
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'].'/campus/partials/_footer.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/campus/partials/_footer.php';
 ?>
