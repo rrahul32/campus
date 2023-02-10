6e85e3306fc5a66d2cf2fc79bbc8d9528b58a027
= <=>
2023-02-10 17:34:34 +0530

    added sorting to students table

--- company/students.php
+++ company/students.php
@@ -7,19 +7,18 @@ if (isset($_SESSION['loggedin'])) {
         header("Location: /campus/admin");
 }
 include_once $_SERVER['DOCUMENT_ROOT'] . '/campus/partials/config.php';
-$cid=$_SESSION['cid'];
-$offered=false;
+$cid = $_SESSION['cid'];
+$offered = false;
 
 //POST job offer
-if(isset($_POST['submit']))
-{
-    $msg=$_POST['message'];
-    $jid=$_POST['jid'];
-    $sid=$_POST['sid'];
-    $sql="INSERT INTO `offeredjobs`(`jid`,`sid`,`message`,`status`)VALUES($jid,$sid,'$msg','pending')";
-    $result=mysqli_query($conn,$sql);
-    if($result)
-    $offered=true;
+if (isset($_POST['submit'])) {
+    $msg = $_POST['message'];
+    $jid = $_POST['jid'];
+    $sid = $_POST['sid'];
+    $sql = "INSERT INTO `offeredjobs`(`jid`,`sid`,`message`,`status`)VALUES($jid,$sid,'$msg','pending')";
+    $result = mysqli_query($conn, $sql);
+    if ($result)
+        $offered = true;
 }
 //end POST 
 
@@ -31,16 +30,37 @@ $rows = mysqli_fetch_all($result);
 //end getting students
 
 //getting posted jobs
-$sql="SELECT `jid`, `jname`, `salary` FROM `job` WHERE `cid`=$cid ORDER BY `jdate` DESC";
-$result= mysqli_query($conn,$sql);
-if($result)
-$jobs=mysqli_fetch_all($result);
+$sql = "SELECT `jid`, `jname`, `salary` FROM `job` WHERE `cid`=$cid ORDER BY `jdate` DESC";
+$result = mysqli_query($conn, $sql);
+if ($result)
+    $jobs = mysqli_fetch_all($result);
 else
-$jobs=null;
+    $jobs = null;
 //end getting posted jobs
 $pageTitle = "Students: Campus Recruitment Management System";
 include_once $_SERVER['DOCUMENT_ROOT'] . '/campus/partials/_template.php';
 ?>
+<style>
+    thead>tr>th {
+        cursor: pointer;
+    }
+
+    .arrow {
+        display: block;
+        height: 24px;
+        width: 24px;
+        margin-right: auto !important;
+        margin-left: auto !important;
+        background-image: url(/campus/images/arrow.svg);
+        background-position: center;
+        background-repeat: no-repeat;
+        transition: transform 0.3s ease-in-out;
+    }
+
+    .up {
+        transform: rotateZ(180deg);
+    }
+</style>
 <div class="container mx-auto mt-3 border p-4 text-center justify-content-center">
     <div class="row">
         <h1><mark style='background-color:#d08181'>Students</mark></h1>
@@ -51,111 +71,173 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/campus/partials/_template.php';
             echo "<div class='alert alert-dismissible alert-success text-center col-10 mx-auto bg-light' role='alert'>Job offer sent successfully!
                 <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
                 </div>";
-                ?>
-                </div>
-<div class="row m-3 p-3 overflow-auto justify-content-center">
-    <?php
-    if ($rows == null)
-        echo "
+        ?>
+    </div>
+    <div class="row m-3 p-3 overflow-auto justify-content-center">
+        <?php
+        if ($rows == null)
+            echo "
     <div class='row p-3 m-3'>
     <h3>No students registered</h3>
     </div>
     ";
-    else 
-    {
-        echo "<table class='table' style='background-color:#d08181'>
+        else {
+            echo "<table class='table' style='background-color:#d08181'>
         <thead>
         <tr>
-        <th scope='col'>S.No.</th>
-      <th scope='col'>Name</th>
-      <th scope='col'>Email</th>
-      <th scope='col'>Course</th>
-      <th scope='col'>Percentage</th>";
-      if($jobs!=null)
-      echo" <th scope='col'>Send an Offer</th>";
-        echo "</tr>
+        <th scope='col' data-desc=true id='idColumn'>S.No. <span class='sort arrow'></span></th>
+      <th scope='col' data-desc=false id='nameColumn'>Name<span class='sort'></span></th>
+      <th scope='col' >Email</th>
+      <th scope='col' data-desc=false id='courseColumn'>Course<span class='sort'></span></th>
+      <th scope='col' data-desc=false id='percentageColumn'>Percentage<span class='sort'></span></th>";
+            if ($jobs != null)
+                echo " <th scope='col'>Send an Offer</th>";
+            echo "</tr>
         </thead>
         <tbody>
         ";
-        $row_num=1;
-        foreach($rows as $row){
-            echo "
+            $row_num = 1;
+            foreach ($rows as $row) {
+                echo "
             <tr>
-            <th scope='row'>$row_num</th>
-            <td>".ucfirst($row[1])." ".ucfirst($row[2])."</td>
-            <td>".$row[0]."</td>
-            <td>".ucwords($row[3])."</td>
+            <td scope='row'>$row_num</td>
+            <td>" . ucfirst($row[1]) . " " . ucfirst($row[2]) . "</td>
+            <td>" . $row[0] . "</td>
+            <td>" . ucwords($row[3]) . "</td>
             <td>$row[4]</td>";
-            $sqljob="SELECT * FROM `offeredjobs` WHERE `sid`=$row[5] AND `jid` IN (SELECT `jid` FROM `job` WHERE `cid`=$cid)";
-            $resultjob= mysqli_query($conn,$sqljob);
-            if($jobs!=null && mysqli_num_rows($resultjob)==0)
-            echo "<td><button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#sendOffer' data-bs-id='$row[5]'>Offer Job</button></td>";
-            else if(mysqli_num_rows($resultjob)>0)
-            {
-                echo "<td>Job offer sent!</td>";
+                $sqljob = "SELECT * FROM `offeredjobs` WHERE `sid`=$row[5] AND `jid` IN (SELECT `jid` FROM `job` WHERE `cid`=$cid)";
+                $resultjob = mysqli_query($conn, $sqljob);
+                if ($jobs != null && mysqli_num_rows($resultjob) == 0)
+                    echo "<td><button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#sendOffer' data-bs-id='$row[5]'>Offer Job</button></td>";
+                else if (mysqli_num_rows($resultjob) > 0) {
+                    echo "<td>Job offer sent!</td>";
+                }
+                echo "</tr>";
+                $row_num++;
             }
-            echo "</tr>";
-          $row_num++;
         }
-    }
 
-    ?>
-    <!-- Modal -->
-<div class="modal fade" id="sendOffer" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <div class="col-4"></div>
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">Job Details</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      <form action="" method="post" name="postjob" onsubmit="return confirm('Are you sure you want to send?')">
-      <div class="row mb-3">
-            <label for="jid" class="col-sm-3 col-form-label">Offered Job</label>
-            <div class="col-sm-9">
-                <select name="jid" id="jid">
-                <?php 
-                if($jobs!=null)
-                foreach($jobs as $job)
-                echo "<option value='$job[0]'>Title:".ucwords($job[1])." <br>Salary:$job[2]</option>";
-                ?>
-                </select>
-            </div>
-      </div>
-            <div class="row mb-3">
-            <label for="message" class="col-sm-2 col-form-label">Message</label>
-            <div class="col-sm-10">
-                <textarea name="message" id="message" cols="30" rows="10" placeholder="Enter the message here..."></textarea>
-            </div>
-            </div>
-        <input type="hidden" name="sid" id="sid">
-        <div class="row mb-3 justify-content-center">
-            <div class="col-3 text-center">
-                <button type="submit" class="btn btn-primary" name="submit" id="submit">
-                    Send Offer
-                </button>
+        ?>
+        <!-- Modal -->
+        <div class="modal fade" id="sendOffer" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <div class="col-4"></div>
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Job Details</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="" method="post" name="postjob" onsubmit="return confirm('Are you sure you want to send?')">
+                            <div class="row mb-3">
+                                <label for="jid" class="col-sm-3 col-form-label">Offered Job</label>
+                                <div class="col-sm-9">
+                                    <select name="jid" id="jid">
+                                        <?php
+                                        if ($jobs != null)
+                                            foreach ($jobs as $job)
+                                                echo "<option value='$job[0]'>Title:" . ucwords($job[1]) . " <br>Salary:$job[2]</option>";
+                                        ?>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label for="message" class="col-sm-2 col-form-label">Message</label>
+                                <div class="col-sm-10">
+                                    <textarea name="message" id="message" cols="30" rows="10" placeholder="Enter the message here..."></textarea>
+                                </div>
+                            </div>
+                            <input type="hidden" name="sid" id="sid">
+                            <div class="row mb-3 justify-content-center">
+                                <div class="col-3 text-center">
+                                    <button type="submit" class="btn btn-primary" name="submit" id="submit">
+                                        Send Offer
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-    </form>
-      </div>
+        <!-- Modal end -->
     </div>
-  </div>
-</div>
-<!-- Modal end -->
 </div>
 </div>
-</div>
+
 <script>
     //modal 
-    const modal= document.getElementById("sendOffer")
-    modal.addEventListener("show.bs.modal",(event)=>{
-        const button=event.relatedTarget
-        const sid=button.getAttribute('data-bs-id')
-        const target=document.getElementById('sid')
-        target.value=sid
+    const modal = document.getElementById("sendOffer")
+    modal.addEventListener("show.bs.modal", (event) => {
+        const button = event.relatedTarget
+        const sid = button.getAttribute('data-bs-id')
+        const target = document.getElementById('sid')
+        target.value = sid
     })
     //modal
+    function tableSort(id = 0, desc = false) {
+        var table = document.querySelector('.table')
+        var rows = table.rows;
+        for (i = 1; i < rows.length - 1; i++) {
+            min = rows[i].getElementsByTagName('td')[id].innerHTML.toLowerCase();
+            minRow = i;
+            for (j = i + 1; j < rows.length; j++) {
+                if (!desc) {
+                    if (min > rows[j].getElementsByTagName('td')[id].innerHTML.toLowerCase()) {
+                        min = rows[j].getElementsByTagName('td')[id].innerHTML.toLowerCase()
+                        minRow = j;
+                    }
+                } else {
+                    if (min < rows[j].getElementsByTagName('td')[id].innerHTML.toLowerCase()) {
+                        min = rows[j].getElementsByTagName('td')[id].innerHTML.toLowerCase()
+                        minRow = j;
+                    }
+                }
+
+            }
+            if (minRow != i) {
+                rows[i].parentNode.insertBefore(rows[minRow], rows[i])
+            }
+        }
+
+    }
+
+    function toggleClick(e, id) {
+        target = e;
+        //console.log(target)
+        document.querySelectorAll('.sort').forEach((ele) => {
+            if (ele.parentElement != target)
+                ele.className = 'sort';
+        })
+
+        if (target.dataset.desc == 'false') {
+            target.querySelector('span').classList.add('arrow')
+            target.querySelector('span').classList.remove('up')
+            tableSort(id, false);
+            target.dataset.desc = 'true';
+        } else {
+            target.querySelector('span').classList.add('up')
+            tableSort(id, true);
+            target.dataset.desc = 'false';
+        }
+    }
+    document.getElementById('idColumn').addEventListener('click', () => {
+        // console.log(e.originalTarget)
+        toggleClick(document.getElementById('idColumn'), 0)
+    })
+    document.getElementById('nameColumn').addEventListener('click', () => {
+         //console.log(e)
+        toggleClick(document.getElementById('nameColumn'), 1)
+
+    })
+    document.getElementById('courseColumn').addEventListener('click', () => {
+        // console.log(e.originalTarget)
+        toggleClick(document.getElementById('courseColumn'), 3)
+    })
+    document.getElementById('percentageColumn').addEventListener('click', () => {
+        // console.log(e.originalTarget)
+        toggleClick(document.getElementById('percentageColumn'), 4)
+    })
 </script>
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/campus/partials/_footer.php';
--- index.php
+++ index.php
@@ -103,7 +103,10 @@ else
                     <div class='card-body' style='background-color:#fdeed2'>
                         <h5 class='card-title'><b>$value[1]</b></h5>
                         <h6 class='card-title'>$value[2]</h6>
-                        <h6 class='card-subtitle mb-2 text-muted'>$value[3]</h6>";
+                        <h6 class='card-subtitle mb-2 text-muted'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-geo-alt' viewBox='0 0 16 16'>
+                        <path d='M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z'/>
+                        <path d='M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z'/>
+                      </svg>$value[3]</h6>";
                     $date = date_diff(date_create("now"), date_create($value[5]));
                     if ($date->format('%m') > 0) {
                         if ($date->format('%m') > 1)
@@ -124,11 +127,14 @@ else
                 }
                 echo "</div>
                 <div class='col-6 sticky-top overflow-auto' id='jobDetail'>
-                    <div class='card mt-3'>
-                        <div class='card-body text-center' style='background-color:#fdeed2'>
+                    <div class='card pt-3' style='min-height:100%;background-color:#fdeed2'>
+                        <div class='card-body text-center'>
                             <h3 class='card-title'><b id='jobTitle'></b></h3>
                             <h5 class='card-title' id='companyName'></h5>
-                            <h6 class='card-subtitle mb-2 text-muted' id='location'></h6>
+                            <h6 class='card-subtitle mb-2 text-muted'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-geo-alt' viewBox='0 0 16 16'>
+                            <path d='M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z'/>
+                            <path d='M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z'/>
+                          </svg><span id='location'></span></h6>
                             <h6 class='card-subtitle mb-2 text-muted' id='salary'></h6>
                             <p class='card-text' id='jobDescription'></p>";
                             if(!isset($_SESSION['type']))
